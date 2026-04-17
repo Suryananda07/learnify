27e0b29cbce24a18f003abe2e54355341f05583a
Suryananda07 <nandasurya500@gmail.com>
2026-04-18 06:57:23 +0800

    final update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,50 +9,6 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function User(Request $request)
-    {
-        $role = $request->role;
-        $view = $request->view;
-        $search = $request->search;
-
-        $query = User::query()
-            ->when($search, function ($query) use ($search) {
-                $query->where(function ($q) use ($search) {
-                    $q->where('name', 'like', "%{$search}%");
-                    $q->orWhere('email', 'like', "%{$search}%");
-                    $q->orWhere('role', 'like', "%{$search}%");
-                });
-            })
-            ->when($role && $role !== 'allUser', function ($query) use ($role) {
-                $query->where('role', $role);
-            });
-            $allUsers = $view === 'all' 
-            ? $query->get()
-            : $query->take(5)->get();
-
-        return view('admin.admin-user', compact('allUsers'));
-    }
-
-    public function userEdit(User $user){
-        return view('admin.admin-user-edit', compact('user'));
-    }
-
-    public function adminCourse(){
-        $courses = Course::paginate(6);
-        return view('admin.admin-course', compact('courses'));
-    }
-
-    public function showCourse(Course $course){
-        return view('admin.admin-course-show');
-    }
-
-    public function adminCourseAdd(){
-        return view('admin.admin-course-add');
-    }
-
-    public function adminCourseEdit(){
-        return view('admin.admin-course-edit');
-    }
 
 public function adminDashboard(Request $request){
     $totalUsers = User::count();
